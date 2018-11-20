4e175180177d4f0b57a9153bb6eb6cfe6ca62f9b
liumapp <liumapp.com@gmail.com>
2018-11-20 10:43:12 +0800

    config admin-server application.yml

---
+++ admin-server/src/main/java/com/liumapp/demo/admin/server/AdminServerMain.java
@@ -0,0 +1,21 @@
+package com.liumapp.demo.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file AdminServerMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/11/20
+ */
+@SpringBootApplication
+public class AdminServerMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerMain.class , args);
+    }
+
+}
