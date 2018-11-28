2e58fbda798a6a8d69cbef5fe3aa7498749bd4eb
liumapp <liumapp.com@gmail.com>
2018-11-28 14:56:17 +0800

    update dependency of admin-client-a

---
+++ admin-client-a/src/main/java/com/liumapp/demo/admin/client/a/AdminClientAMain.java
@@ -0,0 +1,21 @@
+package com.liumapp.demo.admin.client.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * file AdminClientAMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/11/28
+ */
+@SpringBootApplication
+public class AdminClientAMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientAMain.class , args);
+    }
+
+}
