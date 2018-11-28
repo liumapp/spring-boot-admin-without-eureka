5712c690c0a8bde1a13ff9a68c940af0b2454a1b
liumapp <liumapp.com@gmail.com>
2018-11-28 14:41:38 +0800

    rm unnecessary things in admin-client -a

--- admin-server/src/main/java/com/liumapp/demo/admin/server/AdminServerMain.java
+++ admin-server/src/main/java/com/liumapp/demo/admin/server/AdminServerMain.java
@@ -3,7 +3,6 @@ package com.liumapp.demo.admin.server;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 /**
